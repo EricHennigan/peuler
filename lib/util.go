73359864352937f3e33a1d2b7ba41cf8df24f233
Eric Hennigan <eric.hennigan@gmail.com>
2023-12-20 16:24:44 -0800

    Solution to Problem 030: Digit Fifth Powers

--- lib/util.go
+++ lib/util.go
@@ -10,6 +10,17 @@ func DigitSum(n *big.Int) int {
   return s
 }
 
+// produces the reverse of the digits
+// The number 0 yields []
+func RDigits(n int) []int {
+  var d []int
+  for n > 0 {
+    d = append(d, n % 10)
+    n /= 10
+  }
+  return d
+}
+
 func Sum(nums []int) int {
   s := 0
   for _, n := range nums {
